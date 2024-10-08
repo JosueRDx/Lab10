bd0dad24bb97279835f4086638c293a461da6923
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-10-07 19:02:14 -0500

    fix: update base URL IP in SeriesApp.kt

--- app/src/main/java/com/josuerdx/lab10/view/SeriesApp.kt
+++ app/src/main/java/com/josuerdx/lab10/view/SeriesApp.kt
@@ -37,7 +37,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 @Composable
 fun SeriesApp() {
-    val urlBase = "http://192.168.114.205:8000/" // o tu IP si usarás un dispositivo externo
+    val urlBase = "http://192.168.140.205:8000/" // o tu IP si usarás un dispositivo externo
     val retrofit = Retrofit.Builder().baseUrl(urlBase)
         .addConverterFactory(GsonConverterFactory.create()).build()
     val servicio = retrofit.create(SerieApiService::class.java)
